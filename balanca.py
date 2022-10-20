101a72330c74da3f4f1cd275dc45a5ac45b6c0d0
zanlucaslaiz <zanlucaslaiz@gmail.com>
2022-10-20 10:24:05 -0300

    desafios

---
+++ balanca.py
@@ -0,0 +1,4 @@
+# Você tem 9 pesos e sabe que 1 deles é mais pesado que os demais, você precisa descobrir qual é. 
+# Para isso você alugou uma balança de pratos e tem direito de utilizá-la 3 vezes. 
+# Desenvolva um algoritmo que execute a sequência de passos que deve ser realizada.
+
